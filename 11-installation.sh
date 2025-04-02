07cbb57224246046096a586277a78aa601d6227f
nagamani <maniece9999@gmail.com>
2025-04-02 17:33:07 +0530

    variables

--- 11-installation.sh
+++ 11-installation.sh
@@ -15,6 +15,12 @@ if [ $? -ne 0 ]
 then
   echo "git is not installed, going to installed"
   dnf install git -y
+  if [ $? -ne 0 ]
+  then
+   echo "git installation is not success.check it once"
+   exit 1
+  else 
+   echo "git installation is success"
 
 else
   echo "git is already installed, nothing to do"
