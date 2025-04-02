ec786cad486086b53097613d70909ba990dd7ff0
nagamani <maniece9999@gmail.com>
2025-04-02 13:03:00 +0530

    variables

--- 06-array.sh
+++ 06-array.sh
@@ -2,8 +2,8 @@
 
 FRUITS=("APPLE" "KIWI" "ORANGE")
 
-echo "First fruit is: $(FRUITS[0])";
-echo "First fruit is: $(FRUITS[1])";
-echo "First fruit is: $(FRUITS[2])";
+echo "First fruit is: ${FRUITS[0]}";
+echo "First fruit is: ${FRUITS[1]}";
+echo "First fruit is: ${FRUITS[2]}";
 
-echo "First fruit is: $(FRUITS[@])";
+echo "First fruit is: ${FRUITS[@]}";
