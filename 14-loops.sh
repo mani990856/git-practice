bc2a5826cdf6f850c2dfcb0a529d694aef88b910
nagamani <maniece9999@gmail.com>
2025-04-03 13:47:32 +0530

    variables

--- 14-loops.sh
+++ 14-loops.sh
@@ -5,11 +5,12 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 
-CHECK_ROOT(){
+CHECK_ROOT()
+{
     if [ $USERID -ne 0 ]
     then
-        echo "please run this script with root priviliges
-        exit 1
+      echo "please run this script with root priviliges"
+      exit 1
     fi
 }
 
