92aeb027a77e862895ee821f974e3796b53b36a5
nagamani <maniece9999@gmail.com>
2025-04-04 06:53:51 +0530

    variables

--- 15-redirectors.sh
+++ 15-redirectors.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-LOGS_FOLDER="/var/log/shell-scripts"
+LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-TIMESTAMP=$(date +%y-%m-%d-%H-%M-%S)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 mkdir -p $LOGS_FOLDER
 
@@ -12,47 +12,47 @@ G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
 
-CHECK_ROOT()
-{
+CHECK_ROOT(){
     if [ $USERID -ne 0 ]
     then
-      echo -e "$R please run this script with root priviliges $N" &>>$LOG_FILE
-      exit 1
+        echo -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
+        exit 1
     fi
 }
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 is...$R FAILED $N"
+        echo -e "$2 is...$R FAILED $N"  | tee -a $LOG_FILE
         exit 1
     else
-        echo -2 "$2 is...$G SUCCESS $N"
+        echo -e "$2 is... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
 
 USAGE(){
-    echo  -e "$R USAGE:: $N sudo sh 15-redirectors.sh package1 package2..."
+    echo -e "$R USAGE:: $N sudo sh 16-redirectors.sh package1 package2 ..."
     exit 1
-
 }
+
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
 CHECK_ROOT
 
 if [ $# -eq 0 ]
-then 
+then
     USAGE
 fi
 
-for package in $@
+for package in $@ # $@ refers to all arguments passed to it
 do
     dnf list installed $package &>>$LOG_FILE
     if [ $? -ne 0 ]
     then
-      echo "git is not installed, going to install" &>>$LOG_FILE
-      dnf install $package -y &>>$LOG_FILE
-      VALIDATE $? "installing $package"
+        echo "$package is not installed, going to install it.." | tee -a $LOG_FILE
+        dnf install $package -y &>>$LOG_FILE
+        VALIDATE $? "Installing $package"
     else
-      echo -e "$package is already  installed.nothing to do" &>>$LOG_FIL
+        echo -e "$package is already $Y installed..nothing to do $N" | tee -a $LOG_FILE
     fi
 done
